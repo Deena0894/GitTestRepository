84f1002054de705dcf86b64f4a8e2b7721133fe2
deena-4650 <deena-4650@172.22.147.138>
2016-12-16 12:39:36 +0530

    Initial Checin On GitTest.

---
+++ GitTest/src/Main.java
@@ -0,0 +1,16 @@
+//$Id$
+
+public class Main {
+	
+	public static void main(String agrs[]){
+		displayGreeting();
+		Players players = new Players();
+		players.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hi Folks...!!");
+		System.out.println("This is to display some cricket players...!");
+	}
+
+}
---
+++ GitTest/src/Players.java
@@ -0,0 +1,13 @@
+//$Id$
+
+public class Players {
+	
+	String[] players = {"Sachin","Kohli","ABD","Zaheer","Dravid"};
+	
+	public void display(){
+		for(String name : players){
+			System.out.println("Player : "+name);
+		}
+	}
+
+}
