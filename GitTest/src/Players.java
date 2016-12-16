fd0df4d3dc0212b93b576892da5e6112cefd2480
deena-4650 <deena-4650@172.22.147.138>
2016-12-16 14:54:09 +0530

    deleted players display implementation.

--- GitTest/src/Players.java
+++ GitTest/src/Players.java
@@ -5,9 +5,7 @@ public class Players {
 	String[] players = {"Sachin","Kohli","ABD","Zaheer","Dravid"};
 	
 	public void display(){
-		for(String name : players){
-			System.out.println("Player : "+name);
-		}
+		
 	}
 
 }
